bbdb9190902cc3e00b680af5ba46dc71553d60ed
anamsoomro <anamsoomroed@gmail.com>
2020-02-18 08:15:53 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    configure do
+        set :vieiws, "views"
+        set :public_dir, "public"
+    end
+    
+    
+    get "/" do
+        erb :index
+    end
 
 end
